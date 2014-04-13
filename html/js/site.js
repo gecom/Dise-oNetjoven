6a73f0eb316ce750c72e356990cd0bb770b85089
nonikun0711 <argomedoa@gmail.com>
2014-04-13 14:48:35 -0500

    index con responsive

---
+++ html/js/site.js
@@ -0,0 +1,95 @@
+
+
+
+$( ".menu li, .paginate li, .view_options .v1 a, .view_options .v2 a, .view_options .v3 a" ).hover( 
+	function(){
+	$(this).attr('style', 'background-color: '+color+' !important')
+	},
+	function(){
+		if(!$(this).hasClass('active')) { 
+			$(this).attr('style', '')
+		}
+	}
+)
+
+$( ".option_news li a, #footer_nav ul li a" ).hover( 
+	function(){
+	$(this).attr('style', 'color: '+color+' !important')
+	},
+	function(){
+		if(!$(this).hasClass('active')) { 
+			$(this).attr('style', '')
+		}
+	}
+)
+$(".options_menu_fixed .search ").on('click', function(e){
+	e.preventDefault();
+	$('.search_box').toggle();
+	if($('.search_box').hasClass('active')){
+		$(this).attr('style', '')
+		$('.search_box').removeClass('active')
+	} else {
+		$(this).attr('style', 'background-color: '+color+' !important')
+		$('.search_box').addClass('active')
+	}
+	
+})
+
+$(".options .show_menu").on('click', function(e){
+	e.preventDefault();
+	$('.showing_menu').toggle();
+	if($('.show_menu').hasClass('active')){		
+		$('.show_menu').removeClass('active')
+	} else {
+		$('.show_menu').addClass('active')
+	}	
+})
+
+$(".options .search").on('click', function(e){
+	e.preventDefault();
+	$('.options .search_box').toggle();
+	if($(this).hasClass('active')){		
+		$(this).removeClass('active')
+	} else {
+		$(this).addClass('active')
+	}	
+})
+
+$(".options .config").on('click', function(e){
+	e.preventDefault();
+	$('.options .showing_config').toggle();
+	if($(this).hasClass('active')){		
+		$(this).removeClass('active')
+	} else {
+		$(this).addClass('active')
+	}	
+})
+
+
+$( ".options_menu_fixed .config" ).hover( 
+	function(){
+	$(this).attr('style', 'background-color: '+color+' !important')
+	},
+	function(){
+		if(!$(this).hasClass('active')) { 
+			$(this).attr('style', '')
+		}
+	}
+)
+
+
+$(document).scroll(function() {
+		var scrolledHeight = $(document).scrollTop();
+
+		if(scrolledHeight >= 95){
+			$("#main_nav").addClass("menu_fixed");
+		}
+		else{
+			$("#main_nav").removeClass("menu_fixed");	
+		}
+
+
+})
+
+
+
